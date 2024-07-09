224386b545697f36417e52bf94bdc347bdd695cd
AbdoMgdy <abdoman46@hotmail.com>
2024-07-09 11:27:09 +0300

    feat(activity): add csv export

--- app/activity-log/page.tsx
+++ app/activity-log/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import {
+  FaChevronDown,
   FaChevronRight,
   FaCircle,
   FaDownload,
@@ -13,6 +14,7 @@ import {
 import { formatDate } from "./utils";
 import { IoFilter } from "react-icons/io5";
 import { APIResponse, Event } from "../type";
+import { mkConfig, generateCsv, download } from "export-to-csv";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -27,6 +29,7 @@ const ActivityLog = () => {
   const [events, setEvents] = useState<Event[]>([]);
   const [isLive, setIsLive] = useState(false);
   const [isLoadingMore, setIsLoadingMore] = useState(false);
+  const [expandedEventId, setExpandedEventId] = useState<string | null>(null);
 
   const { data, error, mutate } = useSWR<APIResponse>(
     `/api/events?page=${page}&pageSize=10&search=${search}&actorId=${filters.actorId}&targetId=${filters.targetId}&actionId=${filters.actionId}`,
@@ -39,19 +42,19 @@ const ActivityLog = () => {
 
   useEffect(() => {
     if (data) {
-      if (isLive) {
-        setEvents((prevEvents) => {
-          const newEventIds = data.events.map((event) => event.id);
-          const filteredPrevEvents = prevEvents.filter(
-            (event) => !newEventIds.includes(event.id)
-          );
+      setEvents((prevEvents) => {
+        const newEventIds = data.events.map((event) => event.id);
+        const filteredPrevEvents = prevEvents.filter(
+          (event) => !newEventIds.includes(event.id)
+        );
+        console.log(isLive, isLoadingMore);
+        if (isLive && !isLoadingMore)
           return [...data.events, ...filteredPrevEvents];
-        });
-      } else {
-        setEvents((prevEvents) => [...prevEvents, ...data.events]);
-        setIsLoadingMore(false);
-      }
+        else return [...filteredPrevEvents, ...data.events];
+      });
+      setIsLoadingMore(false);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data, isLive]);
 
   useEffect(() => {
@@ -64,8 +67,6 @@ const ActivityLog = () => {
       interval = setInterval(() => {
         mutate();
       }, 5000);
-    } else if (interval) {
-      clearInterval(interval);
     }
     return () => {
       if (interval) clearInterval(interval);
@@ -85,12 +86,28 @@ const ActivityLog = () => {
   };
 
   const exportToCSV = () => {
-    // Implement your CSV export logic here
+    const csvConfig = mkConfig({ useKeysAsHeaders: true });
+    const usefulEventsInfo = events.map((event) => ({
+      actorName: event.actorName,
+      actionName: event.action.name,
+      occurredAt: formatDate(event.occurredAt),
+      targetName: event.targetName,
+      location: event.location,
+      description: event.metadata.description,
+    }));
+    const csv = generateCsv(csvConfig)(usefulEventsInfo);
+    download(csvConfig)(csv);
   };
 
   const loadMore = () => {
     setIsLoadingMore(true);
     setPage((prevPage) => prevPage + 1);
+  };
+
+  const toggleExpand = (eventId: string) => {
+    setExpandedEventId((prevEventId) =>
+      prevEventId === eventId ? null : eventId
+    );
   };
 
   if (error) return <div>Failed to load</div>;
@@ -145,7 +162,8 @@ const ActivityLog = () => {
             {events.map((event) => (
               <div
                 key={event.id}
-                className="hover:bg-gray-100 border-b py-4 px-5 grid grid-cols-3 text-gray-950 text-sm"
+                className="hover:bg-gray-100 border-b py-4 px-5 grid grid-cols-3 text-gray-950 text-sm cursor-pointer"
+                onClick={() => toggleExpand(event.id)}
               >
                 <div className=" flex items-center">
                   <div className="w-6 h-6 bg-gray-300 rounded-full flex items-center justify-center text-white mr-2 bg-gradient-to-br from-orange-400 to-purple-600">
@@ -157,10 +175,40 @@ const ActivityLog = () => {
                 <div className="flex items-center justify-between">
                   <p>{formatDate(event.occurredAt)}</p>
 
-                  <div className="text-gray-300 mr-4 cursor-pointer">
-                    <FaChevronRight />
+                  <div className="text-gray-300 mr-4">
+                    {expandedEventId === event.id ? (
+                      <FaChevronDown />
+                    ) : (
+                      <FaChevronRight />
+                    )}
                   </div>
                 </div>
+                {expandedEventId === event.id && (
+                  <div className="p-5 bg-gray-50">
+                    <div className="grid grid-cols-2 gap-4">
+                      <div>
+                        <h5 className="font-semibold">ACTOR</h5>
+                        <p>Name: {event.actorName}</p>
+                        <p>Email: {event.targetName}</p>
+                        <p>ID: {event.actorId}</p>
+                      </div>
+                      <div>
+                        <h5 className="font-semibold">ACTION</h5>
+                        <p>Name: {event.action.name}</p>
+                        <p>Object: {event.action.name}</p>
+                        <p>ID: {event.action.id}</p>
+                      </div>
+                      <div>
+                        <h5 className="font-semibold">METADATA</h5>
+                        <p>{event.metadata.description}</p>
+                      </div>
+                      <div>
+                        <h5 className="font-semibold">TARGET</h5>
+                        <p>{event.targetName}</p>
+                      </div>
+                    </div>
+                  </div>
+                )}
               </div>
             ))}
           </div>
